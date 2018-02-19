804bc8836a5945c4a19bb4e97ded00467494c8c8
Dhiren Patel <dhiren.patel@weather.com@10.142.53.196>
2018-02-19 14:39:50 -0500

    created

---
+++ display.js
@@ -0,0 +1,14 @@
+const express = require('express')();
+const bodyParser = require('body-parser');
+express.use(bodyParser.json());
+
+
+express.post('/display', function(req, res) {
+    console.log('data received: ', JSON.stringify(req.body));
+    res.send('received ' + req.body + '\n');
+});
+
+express.listen(3000, function() {
+    console.log('Display service is listening on port ' + 3000);
+});
+
